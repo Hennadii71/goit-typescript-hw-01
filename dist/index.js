c6e65770d8a33be3720334e041451fa6243b51fa
Hennadii71 <genadisema@gmail.com>
2023-11-04 22:24:46 +0200

    Update index.ts 2

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,6 @@
 const userName = "Hennadii";
 const age = 52;
 const isActiv = true;
+console.log(age);
+const btn = 'small';
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 const userName: string = "Hennadii";
 const age: number = 52;
 const isActiv: boolean = true; 
+
+console.log(age);
+
+type size = 'small' | 'big';
+
+const btn: size = 'small'
