8f0c33ddab67061ace0061914a129a72db98f480
Hennadii71 <genadisema@gmail.com>
2023-11-04 16:14:28 +0200

    Update html and index.ts

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,4 @@
+const userName = "Hennadii";
+const age = 52;
+const isActiv = true;
 //# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+const userName: string = "Hennadii";
+const age: number = 52;
+const isActiv: boolean = true; 
